7c395a8ef5f69cc65134744c914ebbdffa23c459
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2024-10-22 11:15:48 +0900

    Update 0911_Week 2_Wav file.cpp

--- 0911_Week 2_Wav file.cpp
+++ 0911_Week 2_Wav file.cpp
@@ -1,102 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
 using namespace std;
-
-int Example1() {
-	// Writing 
-	fstream out("test.txt"); // make an output file object
-	if (!out) {
-		cout << "Cannot open test.txt.\n";
-		return -1;
-	}
-
-	out << "R " << 9.9 << endl;
-	out << "T " << 4.9 << "\n";
-	out << "M " << 4.88 << endl;
-
-	out.close();
-	return 0;
-}
-int Example2() {
-	// Reading
-	ifstream xx("test.txt");
-
-	if (!xx) {
-		cout << "Cannot open test.txt file.\n";
-		return -1;
-	}
-
-	char item[20];
-	float num;
-
-	xx >> item >> num;
-	cout << item << " " << num << "\n";
-	xx >> item >> num;
-	cout << item << " " << num << "\n";
-	xx >> item >> num;
-	cout << item << " " << num << "\n";
-
-	xx.close();
- 
-	return 0;
-}
-int Example3() {
-	cout << pow(2, 3);
-	return 0;
-}
-
 #define PI 3.141592
-int Example4() {
-	ofstream file("sine_graph.txt");
-	if (!file) {
-		cout << "Cannot open sine_graph.txt file.\n";
-		return -1;
-	}
-	float t, dt, sint, T, f;
-	f = 220; // 1/220 time = 1 주기
-	dt = 1. / 8000.;
-	T = 10. / 220.;
-	for (t = 0; t < T; t += dt) {
-		sint = sin(2. * PI * f * t);
-		file << t << " " << sint << endl;
-	}
-	file.close();
-}
-int Drill1() {
-	ofstream sig("x(t).txt");
-	if (!sig) {
-		cout << "Cannot open x(t).txt.\n";
-		return -1;
-	}
-
-	float T, dt, f0, fc, xt;
-	f0 = 100; fc = 1000;
-	dt = 1. / 2000.; // 1/dt >= 2fc
-	T = 10. / 100.;
-	for (float t = 0; t < T; t += dt) {
-		xt = sin(2. * PI * f0 * t) * cos(2. * PI * fc * t);
-		sig << t << " " << xt << "\n";
-	}
-	sig.close();
-}
-int Drill2() {
-	ofstream sig("x(t).txt");
-	if (!sig) {
-		cout << "Cannot open x(t).txt.\n";
-		return -1;
-	}
-
-	float T, dt, f0, fc, xt;
-	f0 = 100; fc = 1000;
-	dt = 1. / 8000.;
-	T = 10. / 100.;
-	for (float t = 0; t < T; t += dt) {
-		xt = cos(2. * PI * fc * (1. + sin(2. * PI * f0 * t) / 5. * t));
-		sig << t << " " << xt << "\n";
-	}
-	sig.close();
-}
 
 class waveHeader {
 public:
@@ -117,7 +22,6 @@ int Exercise1() {
 }
 
 // Create wave file that produces "C" note at left speaker and "E" note at right speaker for 5 seconds
-# define PI 3.141592
 // 1. Stereo 써야겠다.
 void Example2() {
 	short* data; // short = 2 bytes(16 bits) -> 이 값 범위로 sinusoidal 값 다 표현

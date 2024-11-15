3bf901d012ad3786a7be496f87974d7f7b5ec4f7
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2024-11-15 14:12:43 +0900

    Update 1113_Week 10_QPSK.cpp

--- 1113_Week 10_QPSK.cpp
+++ 1113_Week 10_QPSK.cpp
@@ -31,7 +31,7 @@ public:
 		number_of_bits = Nsymbols * Nbits;
 	}
 
-	int N; 
+	int N;
 	int Nsymbols; // 심볼의 종류 00,01,10,11 -> 4
 	int Nbits; // [bits / symbol]
 	int data, decoded_data;
@@ -57,11 +57,11 @@ void QPSK::initialize() {
 	// resize 하는 이유 : x-dimension 은 크기가 미정되어있으니 N 차원으로 차원 부여
 	c0.resize(N); c1.resize(N); // vector
 	s.resize(N); n.resize(N); r.resize(N);
+	float A = sqrt(2/N);
 
 	for (int i = 0; i < N; i++) {
 		// 이거 왜 360 도를 N(20)등분하지? -> QPSK 면 4등분해야하는 것 아닌가?
-		c0[i] = sqrt(2 / N) * cos(2 * PI / N * i);
-		c1[i] = sqrt(2 / N) * sin(2 * PI / N * i);
+		c0[i] = A * cos(2 * PI / N * i); c1[i] = A * sin(2 * PI / N * i);
 	}
 	number_of_error_bits = 0;
 	cout << "DM initialize SNR : " << SNR << endl << endl;
@@ -72,7 +72,7 @@ void QPSK::encode() {
 	// c_i : eigenvectors, a_i : 입력값 또는 가중치
 	// s : float vector
 	cout << "[Call encode function]" << endl;
-	s =  a0[data] * c0 + a1[data] * c1;
+	s = a0[data] * c0 + a1[data] * c1;
 	cout << "Input data: " << data << " (a0 : " << a0[data] << " a1: " << a1[data] << ")" << endl << endl;
 }
 void QPSK::noise() {
@@ -86,7 +86,7 @@ int QPSK::countError() {
 	int Xor = data ^ decoded_data; // Xor : 두 값이 서로 다르면 1, 서로 다른 비트 값들에 대하여 다 모아서 이진수로 더해줌.
 	cout << "Sent ^ Decoded : " << data << "^" << decoded_data << " = " << Xor << endl << endl;
 	int count = 0, mask = 1;
-	for (int rep = 0; rep < Nbits; mask *= 2) { // 모든 자리 수 검사
+	for (int rep = 0; rep < Nbits; rep++,mask *= 2) { // 모든 자리 수 검사
 		// Nbits = 2 -> [bits/symbol] : 2개의 비트에 대하여 검사
 		// mask : Xor 값을 2진수로 나타냈을 때 각 자리수를 가리키는 수
 		if ((mask & Xor) != 0) count++; // 0 : Error bit 발생X
@@ -104,14 +104,14 @@ void QPSK::decode() {
 	// (a0,a1) = (A,A),(A,-A),(-A,A),(-A,-A) 중에 하나
 	if (ad0 > 0 && ad1 > 0) decoded_data = 0;
 	else if (ad0 > 0 && !(ad1 > 0)) decoded_data = 1;
-	else if ((ad0 > 0) && ad1 > 0) decoded_data = 2;
+	else if (!(ad0 > 0) && ad1 > 0) decoded_data = 2;
 	else decoded_data = 3;
 
 	cout << "QPSK decoding is done \n" << endl;
 }
 void QPSK::setSymbols() {
 	cout << "[Call setSymbols function]" << endl;
-	Eb = 1; // Energy of Bit signal = 2 * a^2 * T_b
+	Eb = 1.; // Energy of Bit signal = 2 * a^2 * T_b
 	No2 = Eb / SNR; // SNR = 비트 신호 에너지 / (단방향 주파수 노이즈 파워 : N_0 / 2)
 	a = sqrt(Eb); // 2a^2 = 2 Eb
 	sigma = sqrt(No2);
@@ -124,9 +124,9 @@ void QPSK::setSymbols() {
 
 int main() {
 	// Digital Modulation
-	QPSK dm(20, 4, 2, 100.0);
+	QPSK dm(20, 4, 2, 1.0);
 	cout << "[Check allocated values in main]" << endl;
-	cout << "N : "<< dm.N << ", Nbits : " << dm.Nbits << ", SNR :  " << dm.SNR << ", Nsymbols : " << dm.Nsymbols << endl << endl;
+	cout << "N : " << dm.N << ", Nbits : " << dm.Nbits << ", SNR :  " << dm.SNR << ", Nsymbols : " << dm.Nsymbols << endl << endl;
 	cout << endl << endl;
 	for (int i = 0; i < dm.Nsymbols; i++) {
 		cout << "[Start: " << i << "-th symbol encoding / decoding] ========================" << endl << endl;
@@ -145,5 +145,4 @@ int main() {
 	cout << "[Check BER]" << endl;
 	cout << "BER = " << dm.number_of_error_bits / (float)dm.number_of_bits << endl << endl;
 	return 1;
-
 }
